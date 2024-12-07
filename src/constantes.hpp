fef3d6f655341b25d20beb9788cb9b71493664fc
Pandaroux007 <arduinoremi@gmail.com>
2024-12-07 12:39:46 +0100

    correction commentaire

--- src/constantes.hpp
+++ src/constantes.hpp
@@ -11,7 +11,7 @@
 
 constexpr uint8_t PIN_BPS[NBR_ZONES] = {12, 11, 10, 4, 3};
 constexpr uint8_t PIN_RELAIS[NBR_ZONES] = {A3, A2, A1, A0, 13};
-// --------------------------   rouge  //  vert  // bleu   //  jaune  //  cyan
+// --------------------------------------   rouge  //  vert  // bleu   //  jaune  //  cyan
 constexpr uint32_t COULEURS[NBR_ZONES] = {0xFF0000, 0x00FF00, 0x0000FF, 0xFFFF00, 0x00FFFF};
 
 CRGB leds[NBR_ZONES];
