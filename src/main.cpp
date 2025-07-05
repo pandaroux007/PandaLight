29380aff3c6256a559f76da92a44990e8a647d22
Pandaroux007 <arduinoremi@gmail.com>
2025-07-05 18:38:53 +0200

    suppression enum inutile, config des instances de ZoneEclairage via boucle for sur tableau de structure, remplacement des uint8_t en size_t dans les boucles for

--- src/main.cpp
+++ src/main.cpp
@@ -1,46 +1,39 @@
 /*
     Nome : PandaLight
-    Version : 1.1
     Auteur : Pandaroux007
-    Lien : https://github.com/pandaroux007/PandaLight
-    Licence : MIT (voir LICENCE.txt)
-
-    NOTE : Pour plus d'informations à propos de ce programme et du projet,
-    référez-vous au fichier README.md
+    Source : https://github.com/pandaroux007/PandaLight
+    Licence : MIT (LICENCE.txt)
+    Documentation : README.md
 */
 
 #include <Arduino.h>
 #include <SoftwareSerial.h>
 #include <ModbusRTUSlave.h>
 #include <SparkFunBME280.h>
-// fichier programmes
+
 #include "ZoneEclairage.hpp"
 
-BME280 bme280;
-
-// ------------------------------ leds
-constexpr uint8_t NBR_ZONES = 5;
-
-CRGB leds[NBR_ZONES];
-
-constexpr uint32_t HEX_ROUGE = 0xFF0000;
-constexpr uint32_t HEX_VERT = 0x00FF00;
-constexpr uint32_t HEX_BLEU = 0x0000FF;
-constexpr uint32_t HEX_JAUNE = 0xFFFF00;
-constexpr uint32_t HEX_CYAN = 0x00FFFF;
-
 // ------------------------------ zones d'éclairage
-ZoneEclairage eclairages[NBR_ZONES];
-
-enum indexZones : uint8_t
-{
-    //   nom  -  index
-    INDEX_ZONE_1, // 0
-    INDEX_ZONE_2, // 1
-    INDEX_ZONE_3, // 2
-    INDEX_ZONE_4, // 3
-    INDEX_ZONE_5, // 4
+struct ZoneConfig {
+    uint8_t pinRelais;
+    uint8_t pinBouton;
+    uint32_t couleur;
 };
+
+constexpr ZoneConfig zonesConfig[] = {
+    {12, A3, 0xFF0000}, // Rouge
+    {11, A2, 0x00FF00}, // Vert
+    {10, A1, 0x0000FF}, // Bleu
+    {4,  A0, 0xFFFF00}, // Jaune
+    {3,  13, 0x00FFFF}  // Cyan
+};
+
+constexpr size_t NBR_ZONES = sizeof(zonesConfig) / sizeof(zonesConfig[0]);
+ZoneEclairage zones[NBR_ZONES];
+
+// ------------------------------ leds & BME280
+CRGB leds[NBR_ZONES];
+BME280 bme280;
 
 // ------------------------------ bouton général (voir README.md)
 OneButton boutonGeneral;
@@ -48,9 +41,9 @@ OneButton boutonGeneral;
 enum class etatBpGeneral : uint8_t
 {
     HORS_ALLUMAGE = 0,
-    PREMIERE_ZONE = INDEX_ZONE_3,
-    DEUXIEME_ZONE = INDEX_ZONE_4,
-    TROISIEME_ZONE = INDEX_ZONE_5
+    PREMIERE_ZONE = 2,
+    DEUXIEME_ZONE = 3,
+    TROISIEME_ZONE = 4
 } etatCourantGeneral;
 
 void callbackGeneralClick();
@@ -67,7 +60,7 @@ constexpr uint16_t MODBUS_BAUDRATE = 38400;
 constexpr uint8_t SLAVE_ADDR = 200;
 
 void configParDefautHoldingRegisters();
-constexpr uint8_t NBR_HOLDING_REGISTERS = NBR_ZONES * 2; // x2 car deux paramètres/zone : temps de fonctionnement ET temps avant extinction
+const uint8_t NBR_HOLDING_REGISTERS = NBR_ZONES * 2; // x2 car deux paramètres/zone : temps de fonctionnement ET temps avant extinction
 uint16_t holdingRegisters[NBR_HOLDING_REGISTERS]; // R/W de la part du maitre modbus (config)
 constexpr uint8_t NBR_INPUT_REGISTERS = 2;
 uint16_t inputRegisters[NBR_INPUT_REGISTERS]; // R seulement de la part du maitre (capteur)
@@ -86,11 +79,10 @@ void setup()
     FastLED.setBrightness(100);
 
     // Initialisation des zones d'éclairage
-    eclairages[INDEX_ZONE_1].begin(12, A3, &holdingRegisters[INDEX_ZONE_1 * 2], &leds[INDEX_ZONE_1], HEX_ROUGE);
-    eclairages[INDEX_ZONE_2].begin(11, A2, &holdingRegisters[INDEX_ZONE_2 * 2], &leds[INDEX_ZONE_2], HEX_VERT);
-    eclairages[INDEX_ZONE_3].begin(10, A1, &holdingRegisters[INDEX_ZONE_3 * 2], &leds[INDEX_ZONE_3], HEX_BLEU);
-    eclairages[INDEX_ZONE_4].begin(4, A0, &holdingRegisters[INDEX_ZONE_4 * 2], &leds[INDEX_ZONE_4], HEX_JAUNE);
-    eclairages[INDEX_ZONE_5].begin(3, 13, &holdingRegisters[INDEX_ZONE_5 * 2], &leds[INDEX_ZONE_5], HEX_CYAN);
+    for(size_t i = 0; i < NBR_ZONES; ++i)
+    {
+        zones[i].begin(zonesConfig[i].pinRelais, zonesConfig[i].pinBouton, &holdingRegisters[i * 2], &leds[i], zonesConfig[i].couleur);
+    }
     // Initialisation du bouton général
     boutonGeneral.setup(2, INPUT_PULLUP, ACTIVE_LOW);
     boutonGeneral.attachClick(callbackGeneralClick);
@@ -112,9 +104,9 @@ void setup()
 void loop()
 {
     // màj machine à état zones
-    for(uint8_t index = 0; index < NBR_ZONES; index++)
+    for(size_t i = 0; i < NBR_ZONES; ++i)
     {
-        eclairages[index].update();
+        zones[i].update();
     }
 
     updateGeneral();
@@ -126,20 +118,11 @@ void loop()
     inputRegisters[1] = (uint16_t)(humidite * 100);
 
     modbus.poll();
-    /*
-    on recoit depuis la gateway 200 4 00 02 9682 toutes les 3 secondes: c'est l'ADU
-    au début on a 200 (l'adresse du slave) et à la fin on a 9682 un CRC qui permet de valider l'ADU
-    si le slave ou le CRC ne sont pas bon on NE répond PAS pour ne pas encombrer le bus puisque soit le master parle à quelqu'un d'autre, soit la trame est fausse
-    la lib utilise n'ensuite que le PDU avec :
-    - 4 le numéro de fonction pour lire des input register
-    - 00 2 octets pou donner l'adresse de depart dans le tableau des input register que l'on veut lire
-    - 02 2 octets qui donnent le nomnre de registrent que l'on veut lire (ici 2 donc le registrer d'adresse 0 et celui d'adresse 1)
-    */
 }
 
 void configParDefautHoldingRegisters()
 {
-    for(uint8_t zone = 0; zone < NBR_ZONES; ++zone)
+    for(size_t zone = 0; zone < NBR_ZONES; ++zone)
     {
         // DEBUG_PRINT("Paramètrage zone "); DEBUG_VALUE_PRINT(zone); DEBUG_PRINTLN("");
         holdingRegisters[zone * 2]     = TEMPS_FONCTIONNEMENT_TOTAL_DEFAUT;
@@ -159,10 +142,10 @@ void callbackGeneralClickLong()
 {
     etatCourantGeneral = etatBpGeneral::HORS_ALLUMAGE;
 
-    for(uint8_t index = INDEX_ZONE_3; index <= INDEX_ZONE_5; index++)
+    for(uint8_t i = 2; i <= NBR_ZONES; i++)
     {
-        // DEBUG_PRINT("GENERAL > Click long sur la zone "); DEBUG_VALUE_PRINT_TAB(index);
-        eclairages[index].callbackClickLong();
+        // DEBUG_PRINT("GENERAL > Click long sur la zone "); DEBUG_VALUE_PRINT_TAB(i);
+        zones[i].callbackClickLong();
     }
 }
 
@@ -186,19 +169,19 @@ void updateGeneral()
         case etatBpGeneral::PREMIERE_ZONE:
             etatCourantGeneral = etatBpGeneral::DEUXIEME_ZONE;
             // DEBUG_PRINT("GENERAL > Click sur la zone "); DEBUG_VALUE_PRINT_TAB((int)etatBpGeneral::PREMIERE_ZONE);
-            eclairages[(int)etatBpGeneral::PREMIERE_ZONE].callbackClick();
+            zones[(int)etatBpGeneral::PREMIERE_ZONE].callbackClick();
             break;
 
         case etatBpGeneral::DEUXIEME_ZONE:
             etatCourantGeneral = etatBpGeneral::TROISIEME_ZONE;
             // DEBUG_PRINT("GENERAL > Click sur la zone "); DEBUG_VALUE_PRINT_TAB((int)etatBpGeneral::DEUXIEME_ZONE);
-            eclairages[(int)etatBpGeneral::DEUXIEME_ZONE].callbackClick();
+            zones[(int)etatBpGeneral::DEUXIEME_ZONE].callbackClick();
             break;
 
         case etatBpGeneral::TROISIEME_ZONE:
             etatCourantGeneral = etatBpGeneral::HORS_ALLUMAGE;
             // DEBUG_PRINT("GENERAL > Click sur la zone "); DEBUG_VALUE_PRINT_TAB((int)etatBpGeneral::TROISIEME_ZONE);
-            eclairages[(int)etatBpGeneral::TROISIEME_ZONE].callbackClick();
+            zones[(int)etatBpGeneral::TROISIEME_ZONE].callbackClick();
             break;
 
         default:
